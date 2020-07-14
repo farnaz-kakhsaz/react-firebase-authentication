4ee97a90b7db6dcf695a9c17970eaef6e7d7a359
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2020-07-14 21:30:09 +0430

    congigure Sign Out Button with Firebase

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import * as ROUTES from "../../constants/routes";
+import SignOutButton from "../SignOut";
 
 const Navigation = () => (
   <>
@@ -19,6 +20,9 @@ const Navigation = () => (
       </li>
       <li>
         <Link to={ROUTES.ADMIN}>Admin</Link>
+      </li>
+      <li>
+        <SignOutButton />
       </li>
     </ul>
   </>
--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { withFirebase } from "../Firebase";
 
-const SignOut = () => <div>Sign Out</div>;
+const SignOutButton = ({ firebase }) => (
+  <button type="button" onClick={firebase.doSignOut}>
+    Sign Out
+  </button>
+);
 
-export default SignOut;
+export default withFirebase(SignOutButton);
